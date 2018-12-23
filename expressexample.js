20e24df51d3a9f6fb1bed1846f7eb91ba6dfee54
bhuvanaroopa87 <bhuvana.roopa87@gmail.com>
2018-12-23 14:15:33 +0100

    commit

--- expressexample.js
+++ expressexample.js
@@ -7,29 +7,36 @@ app.get('/api/Home',(req,res)=>{
     res.send("Welcome to ECommerce WebSite");
 })
 const products=[
-    {id:1,nameofproduct:"Danone Curd"},
-    {id:2,nameofproduct:"Viva Milk"},
-    {id:3,nameofproduct:"Tilda Basmatic Rice"},
-    {id:4,nameofproduct:"Amul Ghee"},
-    {id:5,nameofproduct:"Baby Milk"}
+    {id:1,name:"product1"},
+    {id:2,name:"product2"},
+    {id:3,name:"product3"},
+    {id:4,name:"product4"},
+    {id:5,name:"product5"}
     ]
 //2.get all products
-    app.get('/api/Products',(req,res)=>{
+    app.get('/api/products',(req,res)=>{
       res.send(products);
 
     })
     //3.get one poduct by Id
-    app.get('/api/Products/:id',(req,res)=>{
+    app.get('/api/products/:id',(req,res)=>{
        const product=products.find(p=> p.id===parseInt(req.params.id));
         //console.log(logger);
         logger.log("Error Message");
         if(!product) res.status(404).send(`product with id = ${req.params.id} is not found`);
         res.send(product);
     })
-    //get product by query parameters
-    app.get('/api/Products/:year/:month/?sortBy=name',(req,res)=>{
-        res.send(req.query);
-
+    //4.get product by query parameters
+    app.get('/api/products/:id/2018',(req,res)=>{
+        console.log("query params:",req.query.filterByName);
+        if(req.query.filterByName){
+            const product=products.find(p=> p.name===req.query.filterByName);
+            if(!product) return res.status(404).send(`product with name= ${req.query.filterByName} is not found`);
+            return res.send(product);
+            }
+      //      res.send(products);
+      
+       
     })
 const port=process.env.ECBPORT || 3000;
 app.listen(port,()=> console.log(`listening on port ${port}...`));
